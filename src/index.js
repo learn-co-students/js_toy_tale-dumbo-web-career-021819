9c1184f67a02c56958cf8464e25decf044010884
James Dorr <jamesdorr3@gmail.com>
2019-04-05 13:46:54 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ let addToy = false
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
+  toggleAddForm()
+})
+
+function toggleAddForm() {
   addToy = !addToy
   if (addToy) {
     toyForm.style.display = 'block'
@@ -13,7 +17,78 @@ addBtn.addEventListener('click', () => {
   } else {
     toyForm.style.display = 'none'
   }
-})
+}
 
 
 // OR HERE!
+
+const toyCollection = document.getElementById('toy-collection')
+
+fetch('http://localhost:3000/toys')
+.then(resp => resp.json())
+.then(toyArray => toyArray.forEach (toy => {
+    toyCollection.innerHTML += `
+  <div class="card" id="${toy.id}">
+    <h2>${toy.name}</h2>
+    <img src=${toy.image} class="toy-avatar" />
+    <p>${toy.likes} Likes </p>
+    <button class="like-btn">Like <3</button>
+  </div>`}))
+
+const addToyForm = document.getElementsByClassName('add-toy-form')[0]
+
+addToyForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const nameText = e.target.name.value;
+  const imageText = e.target.image.value;
+  const toyObject = {
+      name: nameText,
+      image: imageText,
+      likes: 0
+  };
+  toggleAddForm();
+  createNewToy(toyObject)
+  .then(toy => {
+    toyCollection.innerHTML += `
+    <div class="card" id="${toy.id}">
+      <h2>${toy.name}</h2>
+      <img src=${toy.image} class="toy-avatar" />
+      <p>${toy.likes} Likes </p>
+      <button class="like-btn">Like <3</button>
+    </div>`
+  })
+  e.target.name.value = ''
+  e.target.image.value = ''
+})
+
+const createNewToy = (toyObj) => {
+  return fetch('http://localhost:3000/toys/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(toyObj)
+  }).then(response => response.json())
+}
+
+toyCollection.addEventListener('click', function(e) {
+  if (e.target.className === 'like-btn') {
+      let currentButton = e.target;
+      let likeHTML = currentButton.parentElement.querySelector('p');
+      likeCount = /\d+/.exec(likeHTML.innerText)[0];
+      likeCount++
+      increaseLikes(e.target.parentElement.id, likeCount)
+      likeHTML.innerText = `${likeCount} Likes`
+}})
+
+const increaseLikes = (id, likeCount) => {
+  fetch(`http://localhost:3000/toys/${id}`, {
+      method: 'PATCH',
+      headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+      },
+      body: JSON.stringify({"likes": `${likeCount}`})
+  }).then(resp => resp.json())
+}
